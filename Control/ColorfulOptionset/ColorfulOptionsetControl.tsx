200ea30e3ec47f1bed65cb9e4607d070c559228b
Stew Wareing <stew.wareing@capgemini.com>
2025-11-21 13:57:39 +0000

    - Added eslint - Change background color to #f5f5f5 - Added dev dependencies to pacakage.json

--- Control/ColorfulOptionset/ColorfulOptionsetControl.tsx
+++ Control/ColorfulOptionset/ColorfulOptionsetControl.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
 
-import {Dropdown, IDropdownOption} from "@fluentui/react/lib/Dropdown"
-import {initializeIcons} from "@fluentui/react/lib/Icons"
-import { Icon} from "@fluentui/react/lib/Icon";
-import {ISelectableOption} from "@fluentui/react/lib/SelectableOption";
-import {dropdownStyles, myTheme} from "./DropdownStyles";
+import { Dropdown, IDropdownOption } from "@fluentui/react/lib/Dropdown"
+import { initializeIcons } from "@fluentui/react/lib/Icons"
+import { Icon } from "@fluentui/react/lib/Icon";
+import { ISelectableOption } from "@fluentui/react/lib/SelectableOption";
+import { dropdownStyles, myTheme } from "./DropdownStyles";
 
-export interface ISetupSchemaValue{
-  icon ?: string;
-  color ?: string;
-} 
-export interface ISetupSchema{
-  [value: string] : ISetupSchemaValue;           
+export interface ISetupSchemaValue {
+  icon?: string;
+  color?: string;
 }
+/*export interface ISetupSchema {
+  [value: string]: ISetupSchemaValue;
+}*/
 
-export interface IConfig{
+export type ISetupSchema = Record<string, ISetupSchemaValue>;
+
+export interface IConfig {
   jsonConfig: ISetupSchema | undefined;
-  defaultIconName : string;
+  defaultIconName: string;
   sortBy: "TEXT" | "VALUE";
 }
 
@@ -30,13 +32,13 @@ initializeIcons();
 
 
 interface IColorfulOptionsetProperties {
-    rawOptions: ComponentFramework.PropertyHelper.OptionMetadata[];
-    selectedKey: number | null;          
-    onChange: (value: number|null) => void
-    isDisabled : boolean;
-    defaultValue : number | undefined;
-    config: IConfig;
-}  
+  rawOptions: ComponentFramework.PropertyHelper.OptionMetadata[];
+  selectedKey: number | null;
+  onChange: (value: number | null) => void
+  isDisabled: boolean;
+  defaultValue: number | undefined;
+  config: IConfig;
+}
 
 
 
@@ -45,57 +47,60 @@ interface IColorfulOptionsetProperties {
 
 
 //export default class ColorfulOptionsetControl extends React.Component<IColorfulOptionsetProperties, {}> {            
-//export const ColorfulOptionsetControl = ({rawOptions, selectedKey, onChange, isDisabled, defaultValue, config}:IColorfulOptionsetProperties): JSX.Element =>{    
-export const ColorfulOptionsetControl = React.memo(({rawOptions, selectedKey, onChange, isDisabled, defaultValue, config}:IColorfulOptionsetProperties): JSX.Element =>{    
-  console.log("%cEntered control", "color:red");
-  const allOptions = [{Label: "--Select--", Value: -1, Color: "transparent"}, ...rawOptions];	
-  let options = allOptions.map((option : ComponentFramework.PropertyHelper.OptionMetadata ) =>  ({key: option.Value, text : option.Label, data: {color: option.Color}}) )
-  if(config.sortBy==="TEXT"){  
-    options = options.sort((a, b) =>a.text.localeCompare(b.text));
-  }			
+//export const ColorfulOptionsetControl = ({rawOptions, selectedKey, onChange, isDisabled, defaultValue, config}:IColorfulOptionsetProperties): JSX.Element =>{ 
 
-  const _onSelectedChanged = (event: any, option?: IDropdownOption) => {       
-    const val = (option?.key == null || option?.key===-1) ? null : option?.key as number;   
-    onChange(val);           
+export const ColorfulOptionsetControl = React.memo(({ rawOptions, selectedKey, onChange, isDisabled, defaultValue, config }: IColorfulOptionsetProperties): JSX.Element => {
+  console.log("%cEntered control", "color:red");
+  const allOptions = [{ Label: "--Select--", Value: -1, Color: "transparent" }, ...rawOptions];
+  let options = allOptions.map((option: ComponentFramework.PropertyHelper.OptionMetadata) => ({ key: option.Value, text: option.Label, data: { color: option.Color } }))
+  if (config.sortBy === "TEXT") {
+    options = options.sort((a, b) => a.text.localeCompare(b.text));
   }
 
-  const _renderOption =(option: ISelectableOption | undefined, className ?:string) : JSX.Element => {
-    const icon = ((config.jsonConfig && option?.key) ? config.jsonConfig[option?.key]?.icon : config.defaultIconName)  ?? config.defaultIconName;
-    const defaultColor = option?.data?.color || "#ffffff";
-    const color = ((config.jsonConfig && option?.key) ? config.jsonConfig[option?.key]?.color : defaultColor)  ?? defaultColor;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const _onSelectedChanged = (event: any, option?: IDropdownOption) => {
+    const val = (option?.key == null || option?.key === -1) ? null : option?.key as number;
+    onChange(val);
+  }
+
+  const _renderOption = (option: ISelectableOption | undefined, className?: string): JSX.Element => {    
+    const icon = ((config.jsonConfig && option?.key) ? config.jsonConfig[option?.key]?.icon : config.defaultIconName) ?? config.defaultIconName;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const defaultColor = (option?.data?.color ?? "#ffffff") as string;    
+    const color = ((config.jsonConfig && option?.key) ? config.jsonConfig[option?.key]?.color : defaultColor) ?? defaultColor;
     return (
       <div className={className}>
-          <Icon className="colorIcon" style={{color: color, marginRight: "8px"}} iconName={icon} aria-hidden="true" />          
-        <span>{option?.text || ""}</span>
+        <Icon className="colorIcon" style={{ color: color, marginRight: "8px" }} iconName={icon} aria-hidden="true" />
+        <span>{option?.text ?? ""}</span>
       </div>
-    );  
+    );
   }
 
   const _onRenderOption = (option: ISelectableOption | undefined): JSX.Element => {
     return _renderOption(option, "ORBIS_ColorfulOptionset_item")
   };
 
-const _onRenderTitle = (options: IDropdownOption[] | undefined): JSX.Element => {
-    const option = (options || [])[0];
+  const _onRenderTitle = (options: IDropdownOption[] | undefined): JSX.Element => {
+    const option = (options ?? [])[0];
     return _renderOption(option, "option");
-        
+
   };
-     
-    return (
-        <Dropdown        
-            placeHolder="---"
-            options={options}
-            defaultValue={defaultValue || -1}
-            selectedKey={selectedKey}  
-            onRenderTitle = {_onRenderTitle}            
-            onRenderOption = {_onRenderOption}            
-            onChange={_onSelectedChanged}                         
-            disabled={isDisabled} 
-            className="ComboBox"                        
-             styles = {dropdownStyles}
-            theme = {myTheme}           
-        />
-    );
+
+  return (
+    <Dropdown
+      placeholder="---"
+      options={options}
+      defaultValue={defaultValue ?? -1}
+      selectedKey={selectedKey}
+      onRenderTitle={_onRenderTitle}
+      onRenderOption={_onRenderOption}
+      onChange={_onSelectedChanged}
+      disabled={isDisabled}
+      className="ComboBox"
+      styles={dropdownStyles}
+      theme={myTheme}
+    />
+  );
 
 });
 /*, (prev, next)=> {  
@@ -105,8 +110,8 @@ const _onRenderTitle = (options: IDropdownOption[] | undefined): JSX.Element => 
         && prev.defaultValue===next.defaultValue 
         && prev.config===next.config;  
 })   */
-   
 
+ColorfulOptionsetControl.displayName = "ColorfulOptionsetControl";
 
 
 
